c29818ab636a7270919152e65314a38f6c9c8857
Rui Liu <ruiliu89@gmail.com>
2012-09-09 11:42:12 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home Page" do
     it "Should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text=>'Sample App')
+      visit root_path
+      page.should have_selector('h1', :text=>'Welcome to the Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text=>"Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
     it "Should have the content 'help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text=> 'Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text=>"Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -28,14 +28,26 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1',text=>'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                                 :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the h1 'contact'" do
+      visit contact_path
+      page.should have_selector('h1',text:'Contact')
+    end
+
+    it "should have the title 'contact'" do
+      visit contact_path
+      page.should have_selector('title', text:"Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
